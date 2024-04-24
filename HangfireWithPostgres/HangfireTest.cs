cef735a4c83bc3a2fe48a8e7502432cd7d0f2e66
hrushikeshjadhav <50551217+hrushikeshjadhav@users.noreply.github.com>
2024-04-24 16:56:30 +0530

    Add files via upload

---
+++ HangfireWithPostgres/HangfireTest.cs
@@ -0,0 +1,12 @@
+﻿using Hangfire;
+
+namespace HangfireWithPostgres
+{
+    public class HangfireTest
+    {
+        public void Test()
+        {
+            BackgroundJob.Enqueue(() => Console.WriteLine("Background job executed."));
+        }
+    }
+}
---
+++ HangfireWithPostgres/TestHangfireController.cs
@@ -0,0 +1,18 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HangfireWithPostgres
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestHangfireController : ControllerBase
+    {
+        [HttpGet]
+        public void CallJob()
+        {
+            //BackgroundJob.Enqueue(() => Console.WriteLine("Background job executed."));
+            BackgroundJob.Schedule(() => Console.WriteLine("Job scheduled at " + DateTime.Now.AddMinutes(1)), DateTimeOffset.Now.AddMinutes(1));
+        }
+    }
+}
