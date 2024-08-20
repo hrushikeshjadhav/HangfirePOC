8298fbd14249e4fcb9fd885d4f808cc4ea4c8d0a
hrushikeshjadhav <50551217+hrushikeshjadhav@users.noreply.github.com>
2024-08-20 15:36:07 +0530

    Hangfire auth only once request

--- HangfireClassLib/MyAuthorizationFilter.cs
+++ HangfireClassLib/MyAuthorizationFilter.cs
@@ -6,34 +6,47 @@ namespace HangfireClassLib
 {
     public class MyAuthorizationFilter : IDashboardAuthorizationFilter
     {
+        private static bool _isAuthorized = false;
+        private static readonly object _lock = new object();
         public bool Authorize(DashboardContext context)
         {
-            string authHeader = context.GetHttpContext().Request.Headers["Authorization"];
-            if (authHeader != null && authHeader.StartsWith("Basic "))
+            lock (_lock)
             {
-                // Get the encoded username and password
-                var encodedUsernamePassword = authHeader.Split(' ', 2, StringSplitOptions.RemoveEmptyEntries)[1]?.Trim();
-
-                // Decode from Base64 to string
-                var decodedUsernamePassword = Encoding.UTF8.GetString(Convert.FromBase64String(encodedUsernamePassword));
-
-                // Split username and password
-                var username = decodedUsernamePassword.Split(':', 2)[0];
-                var password = decodedUsernamePassword.Split(':', 2)[1];
-
-                // Check if login is correct
-                if (IsAuthorized(username, password))
+                if (_isAuthorized)
                 {
-                    return true;
+                    return true; // Already authorized, skip further checks
                 }
+                string authHeader = context.GetHttpContext().Request.Headers["Authorization"];
+                if (authHeader != null && authHeader.StartsWith("Basic "))
+                {
+                    // Get the encoded username and password
+                    var encodedUsernamePassword = authHeader.Split(' ', 2, StringSplitOptions.RemoveEmptyEntries)[1]?.Trim();
+
+                    // Decode from Base64 to string
+                    var decodedUsernamePassword = Encoding.UTF8.GetString(Convert.FromBase64String(encodedUsernamePassword));
+
+                    // Split username and password
+                    var username = decodedUsernamePassword.Split(':', 2)[0];
+                    var password = decodedUsernamePassword.Split(':', 2)[1];
+
+                    bool isUserAuthorized=false;
+                    // Check if login is correct
+                    if (IsAuthorized(username, password))
+                    {
+                        _isAuthorized = true; // Set the flag to true after successful authorization
+                        
+                        return true;
+                    }
+                    return isUserAuthorized;
+                }
+
+                // Return authentication type (causes browser to show login dialog)
+                context.GetHttpContext().Response.Headers["WWW-Authenticate"] = "Basic";
+
+                // Return unauthorized
+                context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                return false;
             }
-
-            // Return authentication type (causes browser to show login dialog)
-            context.GetHttpContext().Response.Headers["WWW-Authenticate"] = "Basic";
-
-            // Return unauthorized
-            context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
-            return false;
         }
 
         private bool IsAuthorized(string username, string password)
